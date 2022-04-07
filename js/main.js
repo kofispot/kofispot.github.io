54e95b6ec6f3bd22bbbefdbfc7146680f2e83e9f
Kofituo <otuokofi@outlook.com>
2022-04-07 19:43:39 +0000

    Let's begin

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function HeaderDetails(title, image, link, isFeatured) {
+  this.title = title
+  this.image = image
+  this.link = link
+  this.featured = isFeatured ? isFeatured : false
+}
+
+/*
+By default, the header would always contain the welcome image
+This function takes header details class object to create more headers
+* */
+function header() {
+  //first we create the welcome header
+}
